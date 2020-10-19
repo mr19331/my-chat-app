ada9aaa18eb897e59b370ce0c2eec936782ec677
mr19331 <mr19331@gmail.com>
2020-10-19 16:38:17 +0000

    Chinook added

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "<h1>Hello, There!</h1>"
 
-app.run(host=os.getenv('IP'),port=int(os.getenv('PORT'), debug=True)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP'),
+            port=int(os.getenv('PORT')),
+            debug=True)
